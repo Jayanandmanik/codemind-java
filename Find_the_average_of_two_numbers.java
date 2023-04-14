37c440d8c7a79bf0010e9a61c8faa2c925f08b51
Jayanandmanik <98297791+Jayanandmanik@users.noreply.github.com>
2023-04-14 23:15:36 +0530

    import java.util.*; public class codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         float z=(float)(x+y)/2;         System.out.printf("%.4f",z);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        float z=(float)(x+y)/2;
+        System.out.printf("%.4f",z);
+    }
+}
