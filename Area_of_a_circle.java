954b00dd21120df18a21f96d44eb5737b7195a36
Jayanandmanik <98297791+Jayanandmanik@users.noreply.github.com>
2023-04-14 23:29:21 +0530

    import java.util.*; public class codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();
            float p=3.14f;
            float a=x*x;
            System.out.println(p*a);
        }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        
+        float p=3.14f;
+        float a=x*x;
+        System.out.println(p*a);
+    }
+}
